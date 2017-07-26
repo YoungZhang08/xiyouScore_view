4519db350a03da1784ac629b947ec898ebbdb03e
theonezyy <895114856@qq.com>
2017-07-26 22:08:26 +0800

    封装Ajax作后续的数据请求

---
+++ src/js/login.js
@@ -0,0 +1,18 @@
+/**
+ * Created by lenovo on 2017/7/26.
+ */
+
+// var getParams = function () {
+//
+// };
+
+(function () {
+    var options = {
+        user_name : getCookie('username'),
+        password : getCookie('password'),
+        name : getCookie('name'),
+        session : getCookie('session')
+    };
+
+
+})();
